c57b2c38e5910108efa06f712ab9827dedc3fec6
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-06-14 10:42:04 -0300

    Criando método para cadastrar usuários

---
+++ js/register.js
@@ -0,0 +1,61 @@
+let botaoLogin = document.getElementById('btn-login');
+const botalCadastrar = document.getElementById('btn-register');
+
+botaoLogin.addEventListener('click',()=>{
+    window.location.href = '../index.html';
+});
+
+botalCadastrar.addEventListener('click',()=>{
+    const nome = document.getElementById('input-name').value;
+    const email = document.getElementById('input-email').value;
+    const senha = document.getElementById('input-password').value;
+    const confirmSenha = document.getElementById('confirm-password').value;
+
+    if(validarcampos(nome, email, senha, confirmSenha)){
+        const usuario = {
+            name: nome,
+            email: email,
+            password: senha
+        }
+        fetch('http://localhost:8080/api/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(usuario)
+        }).then(response => {
+            if(response.ok){
+                alert('Usuário cadastrado com sucesso!');
+                window.location.href = '../index.html';
+            }
+
+            if(response.status === 409){
+                alert('E-mail já cadastrado, faça seu login');
+            }
+            
+        }).catch(error => {
+            console.error('Erro:', error);
+            console.log('aqui');
+            alert('Erro ao cadastrar usuário. Tente novamente.');
+        });
+    }
+    
+});
+
+function validarcampos(nome, email, senha, confirmSenha){
+    if(nome === '' || email === '' || senha === ''){
+        alert('Preencha todos os campos!');
+        return false;
+    }
+
+    if(email.indexOf('@') === -1 || email.indexOf('.') === -1){
+        alert('Email inválido!');
+        return false;
+    }
+
+    if(senha !== confirmSenha){
+        alert('As senhas precisam ser iguais!');
+        return false;
+    }
+    return true;
+}
